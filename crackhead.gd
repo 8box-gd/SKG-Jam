169cd15f5fbd0eca98a3b23fd2d598ef4e0d1e8b
8Box <textduck@gmail.com>
2025-07-11 22:28:07 -0400

    Fixed Bonk
    Added a collision shape for Crackhead, then fixed it by making it ignore collisions with itself

--- crackhead.gd
+++ crackhead.gd
@@ -10,12 +10,14 @@ enum {WANDERING, TRACKING, WINDUP, CHARGE, BONK, STAB}
 @onready var nav_agent: NavigationAgent3D = $NavigationAgent3D
 @onready var bonk_recovery_timer: Timer = $BonkRecoveryTimer
 @onready var debug_cap: MeshInstance3D = $DebugCap
+@onready var charge_collision: Area3D = $ChargeCollision
 
 @export var player: CharacterBody3D
 @export var tracking_speed := 4.0
 @export var tracking_accel := 8.0
 @export var charge_speed := 20.0
 @export var charge_accel := 20.0
+@export var rotation_speed := 10.0
 
 var move_dir := Vector3.FORWARD
 var state := TRACKING
@@ -29,12 +31,20 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	if player:
 		raycast_origin.look_at(player.global_position + Vector3(0,1,0))
+		if state != CHARGE:
+			charge_collision.look_at(player.global_position + Vector3(0,1,0))
+			charge_collision.rotation.x = 0.0
+			charge_collision.rotation.z = 0.0
 	
 	match state:
 		TRACKING: tracking_state(delta)
 		CHARGE: charge_state(delta)
 		BONK: bonk_state(delta)
 		_: pass
+	
+	if state != BONK:
+		var target_angle := Vector3.FORWARD.signed_angle_to(velocity, Vector3.UP)
+		debug_cap.global_rotation.y = lerp_angle(debug_cap.rotation.y, target_angle, rotation_speed * delta)
 
 func tracking_state(delta: float) -> void:
 	var nav_direction := Vector3.ZERO
@@ -46,6 +56,7 @@ func tracking_state(delta: float) -> void:
 	
 	if charge_cast.is_colliding(): # If you get too close, he auto-charges regardless of sneak
 		if charge_cast.get_collider().is_in_group("Player"):
+			update_player_pos()
 			move_dir = to_local(player.global_position + Vector3(0,1,0))
 			move_dir = move_dir.normalized()
 			state = CHARGE
@@ -53,11 +64,6 @@ func tracking_state(delta: float) -> void:
 func charge_state(delta: float) -> void:
 	velocity = velocity.move_toward(move_dir * charge_speed, charge_accel * delta)
 	move_and_slide()
-	
-	if get_slide_collision(0): #Transition to BONK
-		velocity = -velocity * 0.3
-		bonk_recovery_timer.start()
-		state = BONK
 
 func bonk_state(delta: float) -> void: #Transition out of BONK is done by timer
 	move_dir = -move_dir
@@ -87,3 +93,11 @@ func update_player_pos() -> void:
 	last_known_player_position = player.global_position
 	last_pos_indicator.global_position = last_known_player_position
 	nav_agent.target_position = last_known_player_position # Here so it doesnt update every frame
+
+# CHARGE -> BONK transition happens here
+func _on_charge_collision_body_entered(body: Node3D) -> void:
+	if body == self: return
+	if state != CHARGE: return
+	velocity = -velocity * 0.3
+	bonk_recovery_timer.start()
+	state = BONK
