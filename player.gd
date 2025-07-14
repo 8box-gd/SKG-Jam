8feae4b647a4d13df8210efdda57b6b8b67c6b91
8Box <textduck@gmail.com>
2025-07-14 15:53:50 -0400

    Model Finished
    Model is finished, and fully imported into Godot. Can't believe I forgot to commit everything the whole time.

--- player.gd
+++ player.gd
@@ -4,6 +4,8 @@ extends CharacterBody3D
 @onready var camera: Camera3D = $CamPivot/Camera3D
 @onready var debug_cap: MeshInstance3D = $DebugCap
 @onready var step_timer: Timer = $StepTimer
+@onready var model: Node3D = %JournalistModel
+@onready var anim_player: AnimationPlayer = $JournalistModel/GameRig/AnimationPlayer
 
 signal footstep
 
@@ -17,13 +19,24 @@ var _cam_input_direction := Vector2.ZERO
 var _last_movement_direction := Vector3.FORWARD
 var sneaking := false
 var current_speed := 8.0
+var has_control := false
+var dead := false
 
 func _ready() -> void:
 	current_speed = move_speed
+	
+	# WAKE UP JEFF
+	anim_player.play("GetUp/mixamo_com")
+	await anim_player.animation_finished
+	has_control = true
 
 # Negative Z is forward
 func _physics_process(delta: float) -> void:
-	var raw_input := Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	
+	
+	var raw_input := Vector2.ZERO
+	if has_control:
+		raw_input = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	var forward := camera.global_basis.z
 	var right := camera.global_basis.x
 	
@@ -47,6 +60,7 @@ func _physics_process(delta: float) -> void:
 		_last_movement_direction = move_dir
 	var target_angle := Vector3.FORWARD.signed_angle_to(_last_movement_direction, Vector3.UP)
 	debug_cap.global_rotation.y = lerp_angle(debug_cap.rotation.y, target_angle, rotation_speed * delta)
+	model.global_rotation.y = lerp_angle(model.rotation.y, target_angle, rotation_speed * delta)
 	
 	move_and_slide()
 	
@@ -54,3 +68,10 @@ func _physics_process(delta: float) -> void:
 	if raw_input != Vector2.ZERO and step_timer.time_left == 0.0 and not sneaking:
 		footstep.emit()
 		step_timer.start()
+	
+	if has_control: handle_animations()
+
+func handle_animations() -> void:
+	if sneaking: anim_player.play("SneakWalk/mixamo_com")
+	elif velocity.length() > 0: anim_player.play("run-anim/mixamo_com")
+	else: anim_player.play("Idle/mixamo_com")
