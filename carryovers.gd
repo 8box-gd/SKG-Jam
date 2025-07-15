b2eee54ea9a77186ecb79f9b2ac34781f2df7c42
8Box <textduck@gmail.com>
2025-07-14 20:58:31 -0400

    Starting carryover system
    Starting system to ensure some things like objectives carry between deaths

---
+++ carryovers.gd
@@ -0,0 +1,17 @@
+extends Node
+
+var door_code := "111111" # Default door code should be 111111
+var lit_torches: Array[String]
+
+func _ready() -> void:
+	door_code = roll_door_code()
+
+func roll_door_code() -> String:
+	return "%06d" % randi_range(0, 999999)
+
+# WRITING DOWN FOR LATER: Idea for how Torches could work
+# When you touch a torch, it lights up and adds its node name to the lit_torches array.
+# When it is readied in a new scene, it checks if its name is in lit_torches, then 
+# lights up if it is.
+# All torches will probably necessarily have to be under the same node, to prevent
+# name repeats
